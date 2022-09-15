5ebeb73fff68deb867debd6e4b912f6dafcbf3f3
unknown <rashedul15-1718@diu.edu.bd>
2022-09-15 11:03:25 -0700

    Login Page

---
+++ src/components/Login/Login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div className='flex justify-center h-screen items-center'>
+            <div className="card w-96 bg-primary text-primary-content">
+            <div className="card-body">
+                <h2 className="card-title">Card title!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className="card-actions justify-end">
+                    <button className="btn">Buy Now</button>
+                </div>
+            </div>
+        </div>
+        </div>
+    );
+};
+
+export default Login;
